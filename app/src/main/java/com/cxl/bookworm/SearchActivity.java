a447ef4554b5e85ce8100f2bab7271a2613eaee0
CXL <chenxiaolong0001@foxmail.com>
2019-09-03 17:47:41 +0800

    update search

--- app/src/main/java/com/cxl/bookworm/SearchActivity.java
+++ app/src/main/java/com/cxl/bookworm/SearchActivity.java
@@ -5,18 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.cxl.bookworm.R;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private final String LOG_TAG = "Search Activity Log:";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    // TODO do something
+                    Log.i(LOG_TAG,v.getText().toString());
+//                    getSupportFragmentManager().beginTransaction()
+//                            .replace(R.id.search_page,new )
+                    handled = true;
+                }
+                return handled;
+            }
+        });
     }
 
     @Override
